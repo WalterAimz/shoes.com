e338f724e130dd43fce990397a95bab82a706165
WalterAimz <156527628+WalterAimz@users.noreply.github.com>
2024-01-13 18:28:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function navigate1() {
+    var navigate = "./about.html";
+    window.location.href = navigate;
+}
+function navigate2() {
+    var navigat = "./contact.html";
+    window.location.href = navigat;
+}
